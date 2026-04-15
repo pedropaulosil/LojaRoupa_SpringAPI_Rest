7c0884aac850ad6129229a88d983e70ca6f8851a
Pedro Paulo <pedropaulosilvaoliveira2006@gmail.com>
2026-04-15 12:34:47 -0300

    Add files via upload

---
+++ Loja_Roupa_Project/lojaroupa/lojaroupa/src/main/java/com/example/lojaroupa/model/DadosRoupas.java
@@ -0,0 +1,11 @@
+package com.example.lojaroupa.model;
+
+public record DadosRoupas(
+ String marca,
+ String tamanho,
+ String tipo,
+ int quantidade,
+ double preco)
+{
+
+}
---
+++ Loja_Roupa_Project/lojaroupa/lojaroupa/src/main/java/com/example/lojaroupa/model/RoupaRepository.java
@@ -0,0 +1,6 @@
+package com.example.lojaroupa.model;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+public interface RoupaRepository extends JpaRepository<Roupa, Long> {
+}
