f231b00f61fbab47199221a3fdc9e75ab0626ed1
Pedro Paulo <pedropaulosilvaoliveira2006@gmail.com>
2026-04-15 12:35:06 -0300

    Delete Aula9_Springboot/lojaroupa/lojaroupa directory

--- Aula9_Springboot/lojaroupa/lojaroupa/src/main/java/com/example/lojaroupa/model/RoupaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.lojaroupa.model;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import org.springframework.stereotype.Repository;
-public interface RoupaRepository extends JpaRepository<Roupa, Long> {
-}
